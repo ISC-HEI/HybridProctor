995cbab82c1cbac80e46cd70afbea1f0ccc0e00d
Leny Bressoud <enderastronaute@gmail.com>
2025-10-13 13:40:35 +0200

    Login styling and button

--- app/src/app/admin/auth/page.tsx
+++ app/src/app/admin/auth/page.tsx
@@ -3,6 +3,7 @@
 import Input from "@/components/input";
 import style from "./page.module.scss";
 import { verify } from "./page.server";
+import { LogInIcon } from "lucide-react";
 
 export default function Auth() {
   return (
@@ -13,6 +14,7 @@ export default function Auth() {
           Password
           <Input type="password" name="password" />
         </label>
+        <button type="submit" className={style.btn}>Login <LogInIcon className={style.icon}/></button>
       </form>
     </main>
   )
