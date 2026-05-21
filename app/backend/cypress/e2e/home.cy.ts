2a0bed88e49e695456519ee2c102441292b6316a
Leny Bressoud <enderastronaute@gmail.com>
2026-05-21 10:54:03 +0200

    Remove cypress from backend (migrate to project)

--- app/backend/cypress/e2e/home.cy.ts
+++
@@ -1,8 +0,0 @@
-
-describe("The home page", () => {
-  it("should load", () => {
-    cy.request({ url: "http://localhost:3000" }).should((res) => {
-      expect(res.status).to.eq(200)
-    });
-  })
-})
