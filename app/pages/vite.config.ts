09fbfa3c712c502e9a3f14d538566670dd40ad2b
Leny Bressoud <enderastronaute@gmail.com>
2026-05-22 08:46:42 +0200

    TS path aliases

--- app/pages/vite.config.ts
+++ app/pages/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components")
+    }
+  }
 })
