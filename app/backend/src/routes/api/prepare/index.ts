8a589dc7d8e9bbbc7a316401239d51474a01a2c9
Leny Bressoud <enderastronaute@gmail.com>
2026-06-15 13:08:22 +0200

    change route name

--- app/backend/src/routes/api/prepare/index.ts
+++ app/backend/src/routes/api/prepare/index.ts
@@ -3,7 +3,7 @@ import storage from "@/lib/services/storage";
 import type { DirItem } from "@/lib/types/dirItem";
 import { type Request, type Response } from "express";
 
-export async function prepareDownloadPostHandler(req: Request, res: Response) {
+export async function preparePostHandler(req: Request, res: Response) {
   const items: DirItem[] = req.body.items;
   
   return res.status(200).send(await storage.prepareDownload(items));
